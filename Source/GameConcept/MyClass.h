29800047699135f8da9271ef8f855e5c98cfad06
NavionDJ <82059856+NavionDJ@users.noreply.github.com>
2021-07-22 19:00:42 +0200

    Jenkins build

---
+++ Source/GameConcept/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class GAMECONCEPT_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
